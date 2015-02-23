99efbb74da76ae184a4485dad082350ea84bd798
Peter Cappello <cappello@cs.ucsb.edu>
2015-02-23 12:27:12 -0800

    Tour: Inserted license; eliminated unnecessary extension of Return.

--- cs290bHw3/src/applications/euclideantsp/Tour.java
+++ cs290bHw3/src/applications/euclideantsp/Tour.java
@@ -1,19 +1,35 @@
 /*
- * Here comes the text of your license
- * Each line should be prefixed with  * 
+ * The MIT License
+ *
+ * Copyright 2015 peter.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package applications.euclideantsp;
 
-import api.Task;
-import system.Return;
 import java.util.List;
-import system.SpaceImpl;
 
 /**
  *
  * @author Peter Cappello
  */
-public class Tour extends Return implements Comparable<Tour>
+public class Tour implements Comparable<Tour>
 {
     final private List<Integer> tour;
     final private double cost;
@@ -28,13 +44,7 @@ public class Tour extends Return implements Comparable<Tour>
         this.tour = tour;
         this.cost = cost;
     } 
-
-    @Override
-    public void process(Task task, SpaceImpl space) 
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
+ 
     public List<Integer> tour() { return tour; }
     
     public double cost() { return cost; }
@@ -42,7 +52,6 @@ public class Tour extends Return implements Comparable<Tour>
     @Override
     public int compareTo( Tour tour )
     { 
-        
         return this.cost < tour.cost ? -1 : this.cost > tour.cost ? 1 : 0;
     }
 }
